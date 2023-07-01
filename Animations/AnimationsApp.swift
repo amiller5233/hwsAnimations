26c268265c27bcbc0d49dfe72802783710426787
amiller5233@gmail.com <amiller5233@gmail.com>
2023-06-30 19:46:38 -0700

    create xcode project

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Adam Miller on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
