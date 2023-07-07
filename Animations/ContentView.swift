21e65c754cb9047d7a511f2d51a811bffb723ba9
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-06 20:28:33 -0700

    cool text snake animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    @State private var message = Array("Drag me!")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.red, .yellow]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//                .frame(width: 300, height: 200)
+//                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .ignoresSafeArea()
+            
+            HStack(spacing: 0) {
+                ForEach(0..<message.count) { i in
+                    Text(String(message[i]))
+                        .padding(5)
+                        .font(.title)
+                        .background(.purple)
+                        .offset(dragAmount)
+                        .animation(.default.delay(Double(i) / 20), value: dragAmount)
+                }
+            }
+            .gesture(DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                }
+            )
+            
         }
-        .padding()
+        
     }
 }
 
